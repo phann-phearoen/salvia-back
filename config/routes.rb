dc1bc57cb83eff6eaa1db909558a6cb54e349c7a
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-11-06 11:16:57 +0700

    add devise for Editor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :editors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api, defaults: { format: :json } do
     resources :ad
