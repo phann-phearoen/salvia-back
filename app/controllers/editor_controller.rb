ec68ce1f479c14c6792edd9f0b48ec029a205907
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-11-09 15:53:23 +0700

    signin_able with devise

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,7 +1,12 @@
 class EditorController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:show, :index]
+    skip_before_action : authenticate_editor!, only: [:show, :index]
     def index
         @editors = Editor.all 
         render json: @editors
+    end
+
+    def create 
+        editor = Editor.new(name: params[:name], email: params[:email], password: params[:password])
+        editor.save
     end
 end
