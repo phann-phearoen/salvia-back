f021cc88c0fc9003329ed32bb88bf6e279541662
Phearoen <phannphearin2@gmail.com>
2022-01-06 14:16:47 +0700

    add pagination and order to articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::ArticlesController < ApplicationController
     skip_before_action :doorkeeper_authorize!, except: %i[new]
 
     def index 
-        articles = Article.page(1).per(5)
-        render json: ArticleSerializer.new(articles)
+        @articles = Article.order(:creation_date).page(page|| 1).per(10)
+        render json: ArticleSerializer.new(@articles)
+    end
+
+    private
+    def page
+        Article.page(10).total_pages
     end
 end
