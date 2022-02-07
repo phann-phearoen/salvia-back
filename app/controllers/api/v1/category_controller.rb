d38d42433a150291993baecdd178af2071d922f7
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-02-07 09:23:35 +0700

    reverse sort category by id

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::CategoryController < ApplicationController
     skip_before_action :doorkeeper_authorize!, except: %i[new]
 
     def index
-        page = params[:page] || last_page
+        reverser = Category.order(id: :desc)
+
+        page = params[:page] || 1
         per = params[:per] || 10
-        categories = Category.order(:creation_date).page(page).per(per)
+        categories = reverser.page(page).per(per)
 
         total_count = Category.count
         response = {
@@ -18,10 +20,5 @@ class Api::V1::CategoryController < ApplicationController
         @name = params[:name]
         categories = Category.where("display_name like ? ", "%#{@name}%")
         render json: categories, only: [:display_name, :detail, :slug, :id]
-    end
-    
-    private
-    def last_page
-        Category.page(1).per(10).total_pages
     end
 end
